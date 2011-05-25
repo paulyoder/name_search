923c5ef4c5ad81cf6e87c3d38d08016ba10912ee
Paul Yoder <paulyoder@gmail.com>
2011-05-25 14:17:30 -0500

    moved railtie file to industry standard folder

--- lib/person_search.rb
+++ lib/person_search.rb
@@ -1,5 +1,5 @@
 module PersonSearch
-	require 'person_search/railties/railtie.rb' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+	require 'person_search/railtie.rb' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 	require 'person_search/models/name.rb'
 	require 'person_search/models/name_relationship.rb'
 	require 'person_search/models/name_person_join.rb'
