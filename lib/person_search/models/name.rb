1ac803651c64ffe1cecbafc3da0230f9380bcc63
Paul Yoder <paulyoder@gmail.com>
2011-05-26 07:05:31 -0500

    finished tests for Name.relate_nick_names

--- lib/person_search/models/name.rb
+++ lib/person_search/models/name.rb
@@ -1,15 +1,42 @@
 module PersonSearch
 	class Name < ActiveRecord::Base
 		set_table_name :person_search_names
+		before_create :downcase_value
 
 		belongs_to :nick_name_family, :class_name => 'PersonSearch::NickNameFamily'
 
 		validates :value, :uniqueness => true
 
-		def self.add_nick_name(first_name, second_name)
-			family = NickNameFamily.create
-			Name.create! :value => first_name, :nick_name_family => family
-			Name.create! :value => second_name, :nick_name_family => family
+		def self.relate_nick_names(*names)
+			family = get_family_for_nick_names(names)
+			names.each do |name|
+				name_record = Name.find(name)
+				if name_record.nil?
+					name_record = Name.create! :value => name, :nick_name_family => family
+				elsif name_record.nick_name_family != family
+					name_record.nick_name_family = family
+					name_record.save!
+				end
+			end
+		end
+
+		def self.find(*args)
+			return Name.where(:value => args.first).first if args.first.kind_of?(String)
+			super
+		end
+
+		private
+		
+		def self.get_family_for_nick_names(names)
+			family = Name.where(:value => names.map(&:downcase)).
+										where('nick_name_family_id IS NOT NULL').
+										first.
+										try(:nick_name_family)
+			family ||= NickNameFamily.create
+		end
+
+		def downcase_value
+			value.downcase!
 		end
 	end
 end
--- spec/unit/name_spec.rb
+++ spec/unit/name_spec.rb
@@ -16,30 +16,95 @@ describe PersonSearch::Name do
 	
 	model_has_unique_attributes :value
 
-	context '.add_nick_name' do
-		context 'when neither name is in the database' do
+	it 'always saves name values in downcase' do
+		PersonSearch::Name.delete_all
+		name = Factory :name, :value => 'Paul'
+		name.reload
+		name.value.should == 'paul'
+	end
+
+	describe '.find' do
+		before :all do
+			PersonSearch::Name.delete_all
+			@name = PersonSearch::Name.create! :value => 'joe'
+		end
+		context 'when an integer argument is used' do
+			it 'returns name with same id' do
+				find_name(@name.id).id.should == @name.id
+			end
+		end
+		context 'when a string argument is used' do
+			it 'returns name with same value' do
+				find_name('joe').id.should == @name.id
+			end
+		end
+	end
+
+	describe '.relate_nick_names' do
+		context 'when none of the names are in the database' do
 			before :all do
 				PersonSearch::Name.delete_all
-				PersonSearch::Name.add_nick_name('Sue', 'Suzie')
+				PersonSearch::Name.relate_nick_names('Sue', 'Suzie', 'Susan')
 			end
 				
-			it 'adds both names to the database' do
-				PersonSearch::Name.where(:value => 'Sue').count.should == 1
-				PersonSearch::Name.where(:value => 'Suzie').count.should == 1
+			it 'adds all names to the database' do
+				find_name('sue').should_not be_nil
+				find_name('suzie').should_not be_nil
+				find_name('susan').should_not be_nil
 			end
 
-			it 'both names have the same family' do
-				sue_relationship_id = PersonSearch::Name.where(:value => 'Sue').first.try(:nick_name_family_id)
-				suzie_relationship_id = PersonSearch::Name.where(:value => 'Suzie').first.try(:nick_name_family_id)
+			it 'all names have the same family' do
+				sue_relationship_id = find_name('sue').try(:nick_name_family_id)
+				suzie_relationship_id = find_name('suzie').try(:nick_name_family_id)
+				susan_relationship_id = find_name('susan').try(:nick_name_family_id)
 				sue_relationship_id.should_not be_nil
 				suzie_relationship_id.should_not be_nil
+				susan_relationship_id.should_not be_nil
 				sue_relationship_id.should == suzie_relationship_id
+				sue_relationship_id.should == susan_relationship_id
 			end
 		end
 
-		context 'when first name is in the database' do
-			it 'adds the second name to the database'
-			it 'both names have the same family'
+		context 'when some names are already in the database' do
+			before :all do
+				PersonSearch::Name.delete_all
+				Factory :name, :value => 'joe'
+				PersonSearch::Name.relate_nick_names('joseph', 'joey', 'joe')
+			end
+
+			it 'adds the other names to the database' do
+				find_name('joseph').should_not be_nil
+				find_name('joey').should_not be_nil
+			end
+
+			it 'all names have the same family' do
+				family = find_name('joseph').nick_name_family
+				find_name('joey').nick_name_family.should == family
+				find_name('joe').nick_name_family.should == family
+			end
 		end
+
+		context 'when a name already has a nick_name_family' do
+			before :all do
+				PersonSearch::Name.delete_all
+				@family = Factory :nick_name_family
+				Factory :name, :value => 'Joe', :nick_name_family => @family
+				PersonSearch::Name.relate_nick_names('Joseph', 'Joey', 'Joe')
+			end
+
+			it 'finds the nick_name_family even if argument name is not downcased' do
+				find_name('joey').nick_name_family.should == @family
+			end
+
+			it 'the other names receive the same nick_name_family' do
+				find_name('joseph').nick_name_family.should == @family
+				find_name('joey').nick_name_family.should == @family
+				find_name('joe').nick_name_family.should == @family
+			end
+		end
+	end
+
+	def find_name(name_or_id)
+		PersonSearch::Name.find(name_or_id)
 	end
 end
