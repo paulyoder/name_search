5c2fc3a7d57dd111338639d981180ee7dc83b766
Paul Yoder <paulyoder@gmail.com>
2011-05-27 11:30:17 -0500

    added name_searchables config options

--- lib/person_search/searchable_concerns.rb
+++ lib/person_search/searchable_concerns.rb
@@ -4,17 +4,20 @@ module PersonSearch
 			base.extend ClassMethods
 		end
 
-		def check
+		def sync_name_values
 			true
 		end
 
 		module ClassMethods
 			def full_name_search_on(attribute)
+				write_inheritable_attribute(:person_search_on, attribute)
+				class_inheritable_reader(:person_search_on)
+
 				class_eval do
-					before_save :check
+					has_many :name_searchables, :as => :searchable, :dependent => :destroy, :include => :name, :class_name => 'PersonSearch::NameSearchable'
+					before_save :sync_name_values, :if => "#{attribute}_changed?".to_sym
 				end
 			end
-
 		end
 	end
 end
--- spec/unit/searchable_concerns_spec.rb
+++ spec/unit/searchable_concerns_spec.rb
@@ -6,8 +6,39 @@ describe PersonSearch::SearchableConcerns do
 	end
 
 	describe 'full_name_search_on' do
-		it 'adds a before_save filter on the attribute' do
-			Customer._save_callbacks.select{|x| x.kind == :before && x.filter == :check}.length.should == 1
+
+		describe 'name_searchables' do
+			it 'adds a polymorphic association to name_searchables' do
+				Customer.new.should respond_to :name_searchables
+			end
+			
+			it 'adds a dependent => destroy option to name_searchables' do
+				name = Factory :name
+				customer = Customer.create!
+				searchable = customer.name_searchables.create! :name => name
+				customer.destroy
+				PersonSearch::NameSearchable.where(:id => searchable.id).count.should == 0
+			end
 		end
+
+		it 'adds a before_save filter on the attribute' do
+			Customer._save_callbacks.
+				select{|x| x.kind == :before && 
+									 x.filter == :sync_name_values &&
+									 x.options[:if].first == :name_changed?}.
+				length.should == 1
+		end
+
+		it 'adds person_search_on class instance variable' do
+			Customer.should respond_to :person_search_on
+		end
+
+		it 'sets person_search_on value to the attribute argument' do
+			Customer.person_search_on.should == :name
+		end
+	end
+
+	describe 'sync_name_values' do
+		it 'saves splits the names and saves each one'
 	end
 end
