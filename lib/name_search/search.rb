870ed6d2b2c902f414e58495c0be7a73937bfcd8
Paul Yoder <paulyoder@gmail.com>
2011-06-11 08:20:21 -0500

    completed search options

--- lib/name_search/search.rb
+++ lib/name_search/search.rb
@@ -1,27 +1,38 @@
 module NameSearch
 	class Search < Array
 		def initialize(klass, name, options = {})
-			names = Name.scrub_and_split_name(name)
-			matches = klass.joins(:name_searchables).
-									where(:name_search_searchables => { 
-													:name_id => name_search_name_ids(names) }).
-									all.
-									uniq
-			matches.sort!{|a,b| (b.name_searchable_values & names).length <=> (a.name_searchable_values & names).length }
-			self.concat(matches)
+			name_values = Name.scrub_and_split_name(name)
+      nick_values = (options[:match_mode] == :exact) ? [] : nick_name_values(name_values)
+
+      results = matched_models(klass, name_values + nick_values).
+                  map{|x| SearchResult.new(x, name_values, nick_values) }.
+                  sort{|a,b| b.match_score <=> a.match_score }
+
+      if options.has_key?(:matches_at_least)
+        results = results.delete_if{|x| x.matched_names.length < options[:matches_at_least] } 
+      end
+      
+			self.concat(results)
 		end
 
-		def name_search_name_ids(names)
-			Name.where(:value => names_and_nick_names(names)).select('id').map(&:id)
+    def matched_models(klass, name_values_to_search)
+			klass.joins(:name_searchables).
+			  where(:name_search_searchables => { 
+				  :name_id => name_search_name_ids(name_values_to_search) }).
+				all.
+				uniq
+    end
+
+		def name_search_name_ids(name_values)
+			Name.where(:value => name_values).select('id').map(&:id)
 		end
 
-		def names_and_nick_names(names)
-			results = []
-			names.each do |name|
-				results << name
-				results.concat(Name.find(name).try(:nick_name_values))
-			end
-			results
-		end
+    def nick_name_values(name_values)
+      results = []
+      name_values.each do |name|
+        results.concat(Name.find(name).nick_name_values)
+      end
+      results - name_values
+    end
 	end
 end
--- spec/unit/search_spec.rb
+++ spec/unit/search_spec.rb
@@ -10,8 +10,8 @@ describe NameSearch::Search do
 	end
 
 	describe '.initialize' do
-		def search(klass, name)
-			NameSearch::Search.new(klass, name)
+		def search(klass, name, options = {})
+			NameSearch::Search.new(klass, name, options)
 		end
 
 		before :all do
@@ -31,52 +31,91 @@ describe NameSearch::Search do
 		context 'single name' do
 			it 'should return models with exact same names' do
 				results = search(Customer, 'paul')
-				results.select{|x| x.name == 'Paul'}.length.should == 2
+				results.select{|x| x.model.name == 'Paul'}.length.should == 2
 			end
 		end
 
 		context 'multiple names' do
 			it 'should return models with exact same names' do
 				results = search(Customer, 'ben miller')
-				results.select{|x| x.name == 'Ben Miller'}.length.should == 2
+				results.select{|x| x.model.name == 'Ben Miller'}.length.should == 2
 			end
 
 			it 'should return models with same first name' do
 				results = search(Customer, 'fred')
-				results.select{|x| x.name =~ /Fred/}.length.should == 3
+				results.select{|x| x.model.name =~ /Fred/}.length.should == 3
 			end
 
 			it 'should sort returned models by number of name matches' do
 				results = search(Customer, 'Fred Samuel Smith')
 				results.length.should == 3
-				results.index{|x| x.name == 'Fred Samuel Smith'}.should == 0
+				results.index{|x| x.model.name == 'Fred Samuel Smith'}.should == 0
 			end
 		end
 
 		context 'nicknames' do
 			it 'should return models in same nick name family' do
-				search(Customer, 'ben').select{|x| x.name == 'Benjamin'}.length.should == 1
+				search(Customer, 'ben').select{|x| x.model.name == 'Benjamin'}.length.should == 1
 			end
 
 			it 'should return exact matches before nick name matches' do
 				results = search(Customer, 'benjamin')
-				ben_result_index = results.index{|x| x.name == 'Ben Miller'}
-				benjamin_result_index = results.index{|x| x.name == 'Benjamin'}
+				ben_result_index = results.index{|x| x.model.name == 'Ben Miller'}
+				benjamin_result_index = results.index{|x| x.model.name == 'Benjamin'}
 				benjamin_result_index.should be < ben_result_index
 			end
+    end
+
+    context 'search results' do
+      context 'exact_name_matches' do
+        it 'should include searched names that match' do
+          search(Customer, 'benjamin').map(&:exact_name_matches).flatten.should include('benjamin')
+        end
+
+        it 'should not include searched nick names' do
+          search(Customer, 'benjamin').map(&:exact_name_matches).flatten.should_not include('ben')
+        end
+      end
+
+      context 'nick_name_matches' do
+        it 'should include nick names' do
+          search(Customer, 'benjamin').map(&:nick_name_matches).flatten.should include('ben')
+        end
+
+        it 'should not include searched names' do
+          search(Customer, 'benjamin').map(&:nick_name_matches).flatten.should_not include('benjamin')
+        end
+      end
 		end
 
 		context 'should parse the names argument correctly' do
 			it 'should filter out non-alpha-numeric characters' do
 				results = search(Customer, 'miller,')
-				results.select{|x| x.name =~ /Miller/}.length.should == 2
+				results.select{|x| x.model.name =~ /Miller/}.length.should == 2
 			end
 		end
 
 		describe 'options' do
-			describe ':return_search_results' do
-				
-			end
+      describe 'matches_at_least' do
+        context 'when 2' do
+          it 'should bring back models with 2 or more name matches' do
+            results = search(Customer, 'benjamin miller', :matches_at_least => 2)
+            results.length.should == 2
+            results.first.matched_names.length.should be >= 2
+            results.second.matched_names.length.should be >= 2
+          end
+        end
+      end
+      describe 'match_mode' do
+        context ':exact' do
+          it 'does not search on nick names' do
+            results = search(Customer, 'benjamin', :match_mode => :exact)
+            results.map(&:nick_name_matches).flatten.length.should == 0
+          end
+        end
+      end
 		end
+
+    pending 'class_or_active_relation'
 	end
 end
