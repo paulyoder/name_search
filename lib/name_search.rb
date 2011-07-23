4ac7d05591303af3319f6722cdb529006f1666c6
Paul Yoder <paulyoder@gmail.com>
2011-07-23 12:37:06 -0500

    added version file and cleaned up code to prepare for gem publication

--- lib/name_search.rb
+++ lib/name_search.rb
@@ -1,12 +1,13 @@
 module NameSearch
-  require 'name_search/railtie.rb' if defined?(Rails) && Rails::VERSION::MAJOR == 3
   require 'name_search/models/name.rb'
   require 'name_search/models/nick_name_family.rb'
   require 'name_search/models/nick_name_family_join.rb'
   require 'name_search/models/searchable.rb'
+  require 'name_search/active_relation_search.rb'
   require 'name_search/name_search_on.rb'
   require 'name_search/name_searchables_concerns.rb'
+  require 'name_search/railtie.rb' if defined?(Rails) && Rails::VERSION::MAJOR == 3
   require 'name_search/search.rb'
   require 'name_search/search_result.rb'
-  require 'name_search/active_relation_search.rb'
+  require 'name_search/version.rb'
 end
---
+++ lib/name_search/version.rb
@@ -0,0 +1,3 @@
+module NameSearch
+  VERSION = '0.8.0'
+end
